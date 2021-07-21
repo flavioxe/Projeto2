3eb7d3fe66611d9743d0937057a99e04e5483066
flavioxe <flaviomhenrique@mgail.com>
2021-07-21 15:50:36 -0300

    modal e backdrop feitos

--- src/components/UserList.jsx
+++ src/components/UserList.jsx
@@ -4,18 +4,25 @@ import axios from 'axios';
 export default function UserList(){
 
     // Consumo da API para listar usuários
-    const [usuario, setUsuario] = useState([]);
+    const [usuario, setUsuario] = useState([]); 
+    const [showModal, setShowModal] = useState(false);
+    // Primeiro vem a variável criada dentro da state e depois a função usada pra jogar os valores para a variável
     
     useEffect(() => {
         axios.get(`https://www.mocky.io/v2/5d531c4f2e0000620081ddce`)
         .then(res => {
-            const person = res.data;
-            setUsuario(person);
+            const user = res.data;
+            setUsuario(user);
         })
     },[])
+
+    let mostraModal = (e) => {
+        console.log(e.target.dataset.item)
+        setShowModal(true)
+    }
     
     return(
-        <>
+        <>  {/* tag de separação */}
             {/* Listagem de usuários */}
             {usuario.map((item) =>
                 <div className="boxUser" >
@@ -31,12 +38,15 @@ export default function UserList(){
                         </div>
                     </div>
                     <div>
-                        <button>Pagar</button>
+                        <button onClick={(e) => mostraModal(e, item)}>Pagar</button>
                     </div>
                 </div>
             )}
-            
-            
-        </>
+                <div className="backdrop" onClick={() => setShowModal(false)} style={{display: (showModal ? 'block' : 'none')}}>
+                <div className="modal" style={{display: (showModal ? 'block' : 'none')}}>
+                </div>
+
+            </div>
+        </> /* tag de separação */
     )
 }

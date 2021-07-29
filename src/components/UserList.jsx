ffcd9086a43e4e9c95fcc75a3f6c7b755b0df825
flavioxe <flaviomhenrique@mgail.com>
2021-07-29 17:58:30 -0300

    modal de pagamento finalizado

--- src/components/UserList.jsx
+++ src/components/UserList.jsx
@@ -3,11 +3,28 @@ import axios from 'axios';
 
 export default function UserList(){
 
-    // Consumo da API para listar usuários
     const [usuario, setUsuario] = useState([]); 
     const [showModal, setShowModal] = useState(false);
+    const [selectClient, setSelectClient] = useState({})
     // Primeiro vem a variável criada dentro da state e depois a função usada pra jogar os valores para a variável
-    
+    const cards = [
+        // valid card
+        {
+            id: 0,
+            card_number: '1111111111111111',
+            cvv: 789,
+            expiry_date: '01/18',
+        },
+        // invalid card
+        {
+            id: 1,
+            card_number: '4111111111111234',
+            cvv: 123,
+            expiry_date: '01/20',
+        },
+    ];
+
+    // Consumo da API para listar usuários
     useEffect(() => { /*Evita loop, carrega apenas uma vez*/
         axios.get(`https://www.mocky.io/v2/5d531c4f2e0000620081ddce`)
         .then(res => {
@@ -16,10 +33,15 @@ export default function UserList(){
         })
     },[])
 
-    let mostraModal = (e) => {
-        console.log(e.target.dataset.item)
-        setShowModal(true)
+    // let mostraModal = () => {
+    //     setShowModal(true)
+    // }
+
+    const botaoPagar = (item) => {
+        setShowModal("block")
+        setSelectClient(item)
     }
+
     
     return(
         <>  {/* tag de separação */}
@@ -38,30 +60,37 @@ export default function UserList(){
                         </div>
                     </div>
                     <div>
-                        <button onClick={(e) => mostraModal(e, item)}>Pagar</button>
+                        <button onClick={() => botaoPagar(item)}>Pagar</button>
                     </div>
                 </div>
-            )}
+            )} 
+            {/* fim da listagem dos usuários */}
+            {/* Modal Payment */}
             <div className="backdrop" onClick={() => setShowModal(false)} style={{display: (showModal ? 'block' : 'none')}}>
             </div>
             <div className="modal" style={{display: (showModal ? 'block' : 'none')}}>
                     <div className="headerModal">
-                       Pagamento para <span>Nome</span>
+                       Pagamento para  
+                                   <span>
+                                    {selectClient.name}
+                                   </span>
                     </div>
                     <div className="bodyModal">
                         <input className="inputModal" type="text" placeholder="R$ 0,00"></input>
                         <select className="selectModal">
-                            <option>Cartão com final 1111</option>
-                            <option>Cartão com final 1234</option>
+                            {cards.map(
+                                card => (
+                                    <option value={card.id} key={card.id}>
+                                        Cartão com final {card.card_number.substr(-4)}
+                                    </option>
+                                )
+                            )}
                         </select>
                         <button className="buttonModal">Pagar</button>
                     </div>
             <div className="modalSucess">
                 <div className="headerModal"> Recibo de Pagamento </div>
                 <div className="bodyModalPayment"> O pagamento foi concluído com sucesso.</div>
-            </div>
-            <div className="modalError">
-                <div className="headerModal"> Recibo de Pagamento </div>
                 <div className="bodyModalPayment"> O pagamento <span className="spanError"> não </span> foi concluído com sucesso. </div>
             </div>
             </div>
@@ -70,3 +99,5 @@ export default function UserList(){
 }
 
 // https://www.digitalocean.com/community/tutorials/react-axios-react-pt - POST
+// https://www.npmjs.com/package/react-infinite-scroll-component - INFINTE SCROLL
+

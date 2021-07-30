ca9cb7db8d1738e840767d9114eed7601e6b9c49
flavioxe <flaviomhenrique@mgail.com>
2021-07-30 20:54:46 -0300

    reajuste no css, inicio do desenvolvimento das aplicações do pagamento, rever display dos modais

--- src/components/UserList.jsx
+++ src/components/UserList.jsx
@@ -1,12 +1,17 @@
 import React, { useState, useEffect } from "react";
 import axios from 'axios';
 
-export default function UserList(){
+export default function UserList() {
 
-    const [usuario, setUsuario] = useState([]); 
+    const [usuario, setUsuario] = useState([]);
     const [showModal, setShowModal] = useState(false);
-    const [selectClient, setSelectClient] = useState({})
+    const [selectClient, setSelectClient] = useState({});
+    const [card, setCard] = useState("");
+    const [modalTransaction, setmodalTransaction] = useState("none");
+    const [transaction, setTransaction] = useState("");
+    const [payment, setPayment] = useState("");
     // Primeiro vem a variável criada dentro da state e depois a função usada pra jogar os valores para a variável
+
     const cards = [
         // valid card
         {
@@ -27,31 +32,64 @@ export default function UserList(){
     // Consumo da API para listar usuários
     useEffect(() => { /*Evita loop, carrega apenas uma vez*/
         axios.get(`https://www.mocky.io/v2/5d531c4f2e0000620081ddce`)
-        .then(res => {
-            const user = res.data;
-            setUsuario(user);
-        })
-    },[])
+            .then(res => {
+                const user = res.data;
+                setUsuario(user);
+            })
+    }, [])
 
-    // let mostraModal = () => {
-    //     setShowModal(true)
-    // }
 
     const botaoPagar = (item) => {
         setShowModal("block")
         setSelectClient(item)
     }
 
-    
-    return(
+    const reset = () => {
+        setPayment("")
+        setCard("")
+        setmodalTransaction("none")
+    }
+
+    const enviar = (evt) => {
+        evt.preventDefault(evt)
+
+        const paramJson = {
+            "card_number": cards.card_number,
+            "cvv": cards.cvv,
+            "expiry_date": cards.expiry_date,
+            "destination_user_id": selectClient.id,
+            "value": payment
+        }
+
+        fetch("https://run.mocky.io/v3/533cd5d7-63d3-4488-bf8d-4bb8c751c989", {
+            method: "post",
+            body: paramJson
+        })
+            .then(response => response.json())
+            .then((data) => {
+                console.log(data)
+                setShowModal("none")
+                setmodalTransaction("block")
+
+                if (data.status === "Aprovada") {
+                    setTransaction("Pagamento Concluído Com Sucesso!")
+
+                } else {
+                    setTransaction("Pagamento NÃO foi Concluído com Sucesso!")
+                }
+            })
+    }
+
+
+    return (
         <>  {/* tag de separação */}
             {/* Listagem de usuários */}
             {usuario.map((item) =>
                 <div className="boxUser" >
                     <div>
-                        <img src={item.img} alt="Imagem dousuário"/>
+                        <img className="iconUser" src={item.img} alt="Imagem dousuário" />
                     </div>
-                    <div className="divText">
+                    <div className="infoUser">
                         <div>
                             Nome: {item.name}
                         </div>
@@ -60,39 +98,47 @@ export default function UserList(){
                         </div>
                     </div>
                     <div>
-                        <button onClick={() => botaoPagar(item)}>Pagar</button>
+                        <button className="buttonUser" onClick={() => botaoPagar(item)}>Pagar</button>
                     </div>
                 </div>
-            )} 
-            {/* fim da listagem dos usuários */}
+            )}
+
             {/* Modal Payment */}
-            <div className="backdrop" onClick={() => setShowModal(false)} style={{display: (showModal ? 'block' : 'none')}}>
+            <div className="backdrop" onClick={() => setShowModal(false)} style={{ display: (showModal ? 'block' : 'none') }}>
             </div>
-            <div className="modal" style={{display: (showModal ? 'block' : 'none')}}>
-                    <div className="headerModal">
-                       Pagamento para  
-                                   <span>
-                                    {selectClient.name}
-                                   </span>
+            <div className="modal" style={{ display: (showModal ? 'block' : 'none') }}>
+                <div className="headerModal">
+                    <span className="titleModal"> Pagamento para  </span>
+                    <span className="userNameModal">
+                        {selectClient.name}
+                    </span>
+                </div>
+                <div className="bodyModal">
+                    <input className="inputModal" type="text" placeholder="R$ 0,00"></input>
+                    <select className="selectModal">
+                        {cards.map(
+                            card => (
+                                <option value={card.id} key={card.id}>
+                                    Cartão com final {card.card_number.substr(-4)}
+                                </option>
+                            )
+                        )}
+                    </select>
+                    <div className="buttonsDiv">
+                        <button className="buttonModal" onClick={enviar}>Pagar</button>
+                        {/* <button className="buttonModal" type="reset" onClick={() => setShowModal("none")}>Cancelar</button> */}
                     </div>
-                    <div className="bodyModal">
-                        <input className="inputModal" type="text" placeholder="R$ 0,00"></input>
-                        <select className="selectModal">
-                            {cards.map(
-                                card => (
-                                    <option value={card.id} key={card.id}>
-                                        Cartão com final {card.card_number.substr(-4)}
-                                    </option>
-                                )
-                            )}
-                        </select>
-                        <button className="buttonModal">Pagar</button>
-                    </div>
-            <div className="modalSucess">
-                <div className="headerModal"> Recibo de Pagamento </div>
-                <div className="bodyModalPayment"> O pagamento foi concluído com sucesso.</div>
-                <div className="bodyModalPayment"> O pagamento <span className="spanError"> não </span> foi concluído com sucesso. </div>
+                </div>
             </div>
+            {/* Modal do Recibo de Pagamento         */}
+            <div className="modalTransaction" style={{ display: modalTransaction }} >
+                <div className="headerModalPayment">
+                    <span className="titleModal">Recibo de Pagamento</span>
+                    <span className="closeModal" onClick={() => reset()}> X </span>
+                </div>
+                <div>
+                    <span className="bodyModalPayment"> {transaction}</span>
+                </div>
             </div>
         </> /* tag de separação */
     )
--- src/index.js
+++ src/index.js
@@ -2,11 +2,13 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import UserList from './components/UserList'
+// import Teste from './components/Teste'
 
 const rootDiv = document.getElementById('root');
 
 ReactDOM.render(
   <div>
+    {/* <Teste /> */}
     <UserList />
   </div>,
   rootDiv);
